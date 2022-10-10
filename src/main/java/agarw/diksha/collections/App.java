08c1dd107a89b58fa92832188fca57754df8d96f
Diksha Aggarwal <agarwal.deeksha498@gmail.com>
2022-10-10 13:57:13 +0530

    props

--- src/main/java/agarw/diksha/collections/App.java
+++ src/main/java/agarw/diksha/collections/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(emp1.getMobile());
         System.out.println(emp1.getAddress());
         System.out.println(emp1.getCourses());
+        System.out.println(emp1.getProps());
         System.out.println(emp1);
     }
 }
--- src/main/java/agarw/diksha/collections/Employee.java
+++ src/main/java/agarw/diksha/collections/Employee.java
@@ -2,6 +2,7 @@ package agarw.diksha.collections;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 import java.util.Set;
 
 public class Employee {
@@ -10,22 +11,25 @@ public class Employee {
 	private List<String> mobile;
 	private Set<String> address;
 	private Map<String, String> courses;
+	private Properties props;
 	
-	
-	@Override
-	public String toString() {
-		return "Employee [name=" + name + ", mobile=" + mobile + ", address=" + address + ", courses=" + courses + "]";
-	}
 	public Employee() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public Employee(String name, List<String> mobile, Set<String> address, Map<String, String> courses) {
+	public Employee(String name, List<String> mobile, Set<String> address, Map<String, String> courses,
+			Properties props) {
 		super();
 		this.name = name;
 		this.mobile = mobile;
 		this.address = address;
 		this.courses = courses;
+		this.props = props;
+	}
+	@Override
+	public String toString() {
+		return "Employee [name=" + name + ", mobile=" + mobile + ", address=" + address + ", courses=" + courses
+				+ ", props=" + props + "]";
 	}
 	public String getName() {
 		return name;
@@ -50,7 +54,14 @@ public class Employee {
 	}
 	public void setCourses(Map<String, String> courses) {
 		this.courses = courses;
+	}	
+	public Properties getProps() {
+		return props;
 	}
+	public void setProps(Properties props) {
+		this.props = props;
+	}
+	
 	
 	
 }
