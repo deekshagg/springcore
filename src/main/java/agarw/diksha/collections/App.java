bb11d3935b70a17ae5a92334d8add249229837ca
Diksha Aggarwal <agarwal.deeksha498@gmail.com>
2022-10-10 13:34:21 +0530

    coll

--- src/main/java/agarw/diksha/collections/App.java
+++ src/main/java/agarw/diksha/collections/App.java
@@ -3,15 +3,17 @@ package agarw.diksha.collections;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello Diksha!" );
         ApplicationContext context = new ClassPathXmlApplicationContext("configcoll.xml");
+        Employee emp1 = (Employee) context.getBean("emp1");
+        System.out.println(emp1.getName());
+        System.out.println(emp1.getMobile());
+        System.out.println(emp1.getAddress());
+        System.out.println(emp1.getCourses());
+        System.out.println(emp1);
     }
 }
--- src/main/java/agarw/diksha/springcore/App.java
+++ src/main/java/agarw/diksha/springcore/App.java
@@ -3,10 +3,6 @@ package agarw.diksha.springcore;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
